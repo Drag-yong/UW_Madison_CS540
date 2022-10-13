9ad21b9dc989a02d51c6f46cae96aa8cce217449
Drag-yong <ypark272@wisc.edu>
2022-10-12 19:56:35 -0500

    Update everything
    Update the HW 04 and change the directory name

--- HW 04/Clustering.py
+++ HW 04/Clustering.py
@@ -45,7 +45,7 @@ def calc_features(row):
 
 def hac(features):
     featureLength = len(features)
-    # print('featureLength %d' % featureLength)
+
     # Create an (n − 1) × 4 array or list.
     arr = np.zeros(shape=(featureLength - 1, 4))
 
@@ -56,36 +56,29 @@ def hac(features):
     # Z[i, 2] represents complete linkage distance between Z[i, 0] and Z[i, 1]
     # Z[i, 3] tells the size of the new cluster
     # distance matrix for each point
-    dist = np.full([featureLength * 2, featureLength * 2], -1)
-    for i in range(0, featureLength):
+    dist = np.full([featureLength * 2, featureLength * 2], -1.0)
+
+    # This 2D array will store the distance, also! And no updates
+    # This is just for decreasing the time
+    distConst = np.zeros(shape=(featureLength, featureLength))
+
+    for i in range(featureLength):
         # set ID to every single point
         clusterInfo.update({i: [1, [i]]})
         for j in range(i + 1, featureLength):
             x = np.array([features[i], features[j]])
             # Use numpy.linalg.norm()
-            dist[i][j] = dist[j][i] = np.linalg.norm(x)
-
-    # print('clusterInfo')
-    # print(clusterInfo)
+            dist[i][j] = dist[j][i] = distConst[i][j] = distConst[j][i] = np.linalg.norm(
+                abs(features[i] - features[j]))
 
     # Iterate through this array/list row by row.
-    # arrIndex = 0
-    check = 0
     index = featureLength  # This index will update the new cluster.
 
     for row in arr:
-        # if clusterInfo.items == 1:
-        #     break
-        # print('=' * 20)
-        # print("check %d" % (check))
-        # check += 1
-        # print("dist")
-        # print(dist)
-
         # Determine which two clusters you should merge and put their numbers into the
         # first and second elements of the row, Z[i, 0] and Z[i, 1] and Z[i, 0] < Z[i, 1].
         min = -1
-        for i in range(0, featureLength * 2):
+        for i in range(featureLength * 2):
             for j in range(i + 1, featureLength * 2):
                 # This ith jth cluster is used
                 if dist[i][j] == -99:
@@ -101,18 +94,13 @@ def hac(features):
                         feature1 = i
                         feature2 = j
 
-        # print("feature1 = %d, feature = %d" % (feature1, feature2))
-
         row[0] = feature1
         row[1] = feature2
         row[2] = min
         row[3] = clusterInfo[feature1][0] + clusterInfo[feature2][0]
 
-        # print('row')
-        # print(row)
-
         # Update the distance matrix that used.
-        for i in range(0, featureLength * 2):
+        for i in range(featureLength * 2):
             dist[feature1][i] = -99
             dist[feature2][i] = -99
         for i in dist:
@@ -138,18 +126,13 @@ def hac(features):
                 # Access every nodes in the cluster
                 for firstItem in clusterInfo[i][1]:
                     for secondItem in clusterInfo[index][1]:
-                        x = np.array([firstItem, secondItem])
-                        # Use numpy.linalg.norm()
-                        v = np.linalg.norm(x)
-
-                        if v > dist[i][index]:
+                        v = distConst[firstItem][secondItem]
+                        # Set the maximum value of the distance between two clusters
+                        if i != index and v > dist[i][index]:
                             dist[i][index] = dist[index][i] = v
 
         # Update the cluster ID
         index += 1
-        # print("index %d" % index)
-        # print('clusterInfo')
-        # print(clusterInfo)
 
     return arr
 
